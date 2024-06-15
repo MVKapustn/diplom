32876ddb2b518137976cbafda317efd60de5b223
Максим К <@HOME-PC>
2024-06-15 19:14:06 +0300

    Наконец доделал все курсы! Ура!

--- Pages/BaseCoursePg.xaml.cs
+++ Pages/BaseCoursePg.xaml.cs
@@ -15,16 +15,35 @@ namespace DiplomKapustinMaximISP_41.Pages
             switch (PagesHelper.SkillLevel)
             {
                 case PagesHelper.Skill.Low:
-                    /*tabControlLow.Visibility = System.Windows.Visibility.Visible;
-                    tabControlLow.IsEnabled = true;*/
+
+                    tabControlLow.Visibility = System.Windows.Visibility.Visible;
+                    tabControlLow.IsEnabled = true;
+
+                    tabControlMed.Visibility = System.Windows.Visibility.Hidden;
+                    tabControlMed.IsEnabled = false;
+
+                    tabControlHigh.Visibility = System.Windows.Visibility.Hidden;
+                    tabControlHigh.IsEnabled = false;
                     break;
                 case PagesHelper.Skill.Medium:
+                    tabControlLow.Visibility = System.Windows.Visibility.Hidden;
+                    tabControlLow.IsEnabled = false;
+
                     tabControlMed.Visibility = System.Windows.Visibility.Visible;
                     tabControlMed.IsEnabled = true;
+
+                    tabControlHigh.Visibility = System.Windows.Visibility.Hidden;
+                    tabControlHigh.IsEnabled = false;
                     break;
                 case PagesHelper.Skill.High:
-                    /*tabControlHigh.Visibility = System.Windows.Visibility.Visible;
-                    tabControlHigh.IsEnabled = true;*/
+                    tabControlLow.Visibility = System.Windows.Visibility.Hidden;
+                    tabControlLow.IsEnabled = true;
+
+                    tabControlMed.Visibility = System.Windows.Visibility.Hidden;
+                    tabControlMed.IsEnabled = false;
+
+                    tabControlHigh.Visibility = System.Windows.Visibility.Visible;
+                    tabControlHigh.IsEnabled = true;
                     break;
             }
         }   
